0c25b8f0b0b59623f2eb74122f0ebd0661d782f6
sozkaynak <s.ozkaynak@outlook.com>
2019-04-27 11:35:56 +0300

    feat(cors)settings django cors header

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -26,6 +26,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_WHITELIST = (
+    'localhost:4200'
+)
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'post',
     'rest_framework',
-    'ckeditor'
+    'ckeditor',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -48,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'blog.urls'
